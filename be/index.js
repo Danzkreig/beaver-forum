345375df037e45689522e9f2d462af19cdcbf4ea
Termina <festeringwound@proton.me>
2024-02-26 18:15:21 +0800

    changed

--- be/index.js
+++ be/index.js
@@ -1,53 +1,76 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 const app = express();
 const port = 8080;
 app.use(cors());
 app.use(express.json());
-let arr = [
-  {
-    name: "Admin",
-    age: 13,
-    password: "Forgotten",
-    id: 1,
-  },
-  {
-    name: "rappa2004",
-    age: 20,
-    password: "newyork2011",
-    id: 2,
-  },
-  {
-    name: "kingston333",
-    age: 23,
-    password: "robotdog330",
-    id: 3,
-  },
-];
-var identify = 4;
+function getData() {
+  let data = fs.readFileSync("poop.json").toString();
+  let parsed = JSON.parse(data);
+  console.log(parsed);
+  return parsed;
+}
+function editData(type, data) {
+  var main = getData();
+  if (type === "post") {
+    main.push(JSON.parse(data));
+    fs.writeFile("poop.json", JSON.stringify(main), (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Post working");
+      }
+    });
+  } else if (type === "delete") {
+    main = main.filter((obj) => obj.id != JSON.parse(data));
+    fs.writeFile("poop.json", JSON.stringify(main), (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("all good");
+      }
+    });
+  } else if (type === "patch") {
+    main = JSON.stringify(main);
+    main = JSON.parse(main);
+    let pro = JSON.parse(data);
+    let objIndex = main.findIndex((obj) => obj.id == pro.id); // TODO: Find better way.
+    main[objIndex].name = pro.name;
+    main[objIndex].age = parseInt(pro.age);
+    main[objIndex].password = pro.password;
+    fs.writeFile("poop.json", JSON.stringify(main), (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("all good");
+      }
+    });
+  }
+}
+var stringed = JSON.stringify(getData());
+var usable = JSON.parse(stringed);
+var identify = parseInt(usable[usable.length - 1].id) + 1;
 app.get("/users", (request, response) => {
   response.statusCode = 200;
-  response.json(arr);
-  console.log(arr);
+  response.json(getData());
+  console.log("Was Get");
 });
 app.post("/users", (request, response) => {
   let shaft = request.body;
   shaft.id = identify;
   identify++;
-  arr.push(shaft);
+  editData("post", JSON.stringify(shaft));
+  console.log(identify);
   response.sendStatus(200);
 });
 app.delete("/users", (request, response) => {
   let name = request.body.id;
-  arr = arr.filter((co) => co.id != name);
-  console.log(arr);
+  editData("delete", JSON.stringify(name));
   response.sendStatus(200);
 });
 app.patch("/users", (request, response) => {
-  let objIndex = arr.findIndex((obj) => obj.id == request.body.id);
-  arr[objIndex].name = request.body.name;
-  arr[objIndex].age = request.body.age;
-  arr[objIndex].password = request.body.password;
+  editData("patch", JSON.stringify(request.body));
   console.log(request.body);
   response.sendStatus(200);
 });
